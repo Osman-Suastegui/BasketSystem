94d048d0b56558aa23e5459aad990b60ee7279d2
Osman <osman_suastegui@hotmail.com>
2025-02-16 19:04:10 -0700

    Now, single-elimination tournament support any number of matches

--- src/main/java/com/basket/BasketballSystem/matches/SingleEliminationBracket.java
+++ src/main/java/com/basket/BasketballSystem/matches/SingleEliminationBracket.java
@@ -2,7 +2,6 @@ package com.basket.BasketballSystem.matches;
 
 import com.basket.BasketballSystem.exceptions.BadRequestException;
 import com.basket.BasketballSystem.teams.Team;
-import com.basket.BasketballSystem.teams.TeamRepository;
 import com.basket.BasketballSystem.teams_tournaments.TeamTournamentRepository;
 import com.basket.BasketballSystem.tournaments.Tournament;
 import com.basket.BasketballSystem.tournaments.TournamentRepository;
@@ -40,13 +39,39 @@ public class SingleEliminationBracket implements MatchGenerator {
         // once the matches have ids when can link one match to antoher by id;
         Map<Integer, List<Match>> roundToMatches = linkMatchRounds(matches);
 
-        // the matches are linked but we need to fill up the matches with teams in the last level
-        int lastPhase = determinePhase(teams.size());
+        int lastPhase = determinePhase(teams.size() - 2);
+
+        // the matches are linked, but we need to fill up the matches with teams in the last level
+        if(isPowerOfTwo(teams.size())){
+            List<Match> matchesLastRound = roundToMatches.get(lastPhase);
+            assignTeamsToRound(matchesLastRound,teams);
+
+            matches = flattenRounds(roundToMatches);
+            return partidoRepository.saveAll(matches);
+        }
+        // when it's not power of two the matches could be in the last round or penultimate round
+
+        List<Match> matchesPenultimateRound = roundToMatches.get(lastPhase - 1);
+        matchesPenultimateRound.forEach(match ->
+                System.out.println(match.getClavePartido())
+        );
         List<Match> matchesLastRound = roundToMatches.get(lastPhase);
-        assignTeamsToLastRound(matchesLastRound,teams);
+        assignTeamsToRound(matchesPenultimateRound,teams);
+//        // Create a Set of all teams that participated in a match for quick lookup
 
+        Set<Team> teamsInMatches = matchesPenultimateRound.stream()
+                .flatMap(match -> Arrays.stream(new Team[]{match.getEquipo1(), match.getEquipo2()}))
+                .collect(Collectors.toSet());
+
+        // Filter out teams that are already in matches
+        List<Team> teamsNotAssignedToMatch = teams.stream()
+                .filter(team -> !teamsInMatches.contains(team))
+                .toList();
+
+//        // Printing the result
+        teamsNotAssignedToMatch.forEach(team -> System.out.println(team.getName()));
+        assignTeamsToRoundDifferentNumbers(matchesLastRound,teamsNotAssignedToMatch);
         matches = flattenRounds(roundToMatches);
-
         return partidoRepository.saveAll(matches);
     }
 
@@ -78,7 +103,9 @@ public class SingleEliminationBracket implements MatchGenerator {
             if (phase > 1 && !previousRoundMatches.isEmpty()) {
                 for (int i = 0, j = 0; i < currentRoundMatches.size() && j < previousRoundMatches.size(); i += 2, j++) {
                     currentRoundMatches.get(i).setNextMatch(previousRoundMatches.get(j));
-                    currentRoundMatches.get(i + 1).setNextMatch(previousRoundMatches.get(j));
+                    if(i + 1 < currentRoundMatches.size() ){
+                        currentRoundMatches.get(i + 1).setNextMatch(previousRoundMatches.get(j));
+                    }
                 }
             }
             previousRoundMatches = currentRoundMatches;
@@ -87,22 +114,46 @@ public class SingleEliminationBracket implements MatchGenerator {
         return roundToMatches;
     }
 
-    private void assignTeamsToLastRound(List<Match> matches, List<Team> teams) {
+    private void assignTeamsToRound(List<Match> matches, List<Team> teams) {
         // Assume firstRoundMatches size is even and teams list is in proper order
         int teamIndex = 0;
-        for (int i = 0; i < matches.size(); i += 2) {
+        for (int i = 0; i < matches.size(); i ++) {
             Match match1 = matches.get(i);
-            Match match2 = matches.get(i + 1);
             match1.setEquipo1(teams.get(teamIndex));
-            match1.setEquipo2(teams.get(teamIndex + 1));
-            match2.setEquipo1(teams.get(teamIndex + 2));
-            match2.setEquipo2(teams.get(teamIndex + 3));
-            teamIndex += 4;
+            if(teamIndex + 1 < teams.size()){
+                match1.setEquipo2(teams.get(teamIndex + 1));
+            }else{
+                break;
+            }
+            teamIndex += 2;
         }
     }
 
+    private void assignTeamsToRoundDifferentNumbers(List<Match> lastRoundMatches, List<Team> teams){
+
+        int indexTeams = 0;
+        for(int i = 0 ; i < lastRoundMatches.size();  i++){
+            Match match = lastRoundMatches.get(i);
+            Match nextMatch = match.getNextMatch();
+            if(nextMatch.getEquipo1() != null){
+                Team team = nextMatch.getEquipo1();
+                match.setEquipo1(teams.get(indexTeams));
+                match.setEquipo2(team);
+                nextMatch.setEquipo1(null);
+            }else {
+                Team team = nextMatch.getEquipo2();
+                match.setEquipo1(teams.get(indexTeams));
+                match.setEquipo2(team);
+                nextMatch.setEquipo2(null);
+            }
+            indexTeams++;
+        }
+
+    }
+
     private boolean canGenerateBracket(int teamSize) {
-        return teamSize == 2 || teamSize == 4 || teamSize == 8 || teamSize == 16 || teamSize == 32 || teamSize == 64;
+        return true;
+//        return teamSize == 2 || teamSize == 4 || teamSize == 5  || teamSize == 8 || teamSize == 16 || teamSize == 32 || teamSize == 64;
     }
 
     private List<Match> flattenRounds(Map<Integer, List<Match>> rounds) {
@@ -119,6 +170,11 @@ public class SingleEliminationBracket implements MatchGenerator {
         if (matchIndex <= 30) return 5;
         if (matchIndex <= 62) return 6;
         return 0;
+    }
+
+    private boolean isPowerOfTwo(int n){
+        return (n > 0) && ((n & (n - 1)) == 0);
+
     }
 
 
